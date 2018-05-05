e17b60be41645e8091a31a471a5325195f34e929
Paulo Linhares <paulolinhares@mbp-de-macbook.intra.fazenda.sp.gov.br>
2018-05-05 17:13:11 -0300

    asplash ok

--- app/src/main/java/com/hpr/becuniversity/SplashActivity.kt
+++ app/src/main/java/com/hpr/becuniversity/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.hpr.becuniversity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,6 +8,10 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        val intent = Intent(this, MainActivity::class.java)
+        //Intent intent = new Intent(this, TesteFavoritoHistorico.class);
+        startActivity(intent)
+        finish()
     }
 }
