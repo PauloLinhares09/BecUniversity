f6d51e543f969b1cad299a14ea608e262dbcec71
Paulo Linhares <paulolinhares@mbp-de-macbook.intra.fazenda.sp.gov.br>
2018-05-05 16:40:18 -0300

    icons chnage color

--- app/src/main/java/com/hpr/becuniversity/MainActivity.kt
+++ app/src/main/java/com/hpr/becuniversity/MainActivity.kt
@@ -10,15 +10,16 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                message.setText(R.string.enables)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                message.setText(R.string.in_run)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                message.setText(R.string.done)
                 return@OnNavigationItemSelectedListener true
             }
         }
