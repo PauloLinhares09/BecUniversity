3bdb324eb48ad1e53c857fe332cf9617706977fe
Paulo Linhares <paulolinhares@mbp-de-macbook.intra.fazenda.sp.gov.br>
2018-05-05 17:23:15 -0300

    icon menu main ok

--- app/src/main/java/com/hpr/becuniversity/MainActivity.kt
+++ app/src/main/java/com/hpr/becuniversity/MainActivity.kt
@@ -3,6 +3,8 @@ package com.hpr.becuniversity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,5 +33,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/hpr/becuniversity/ShowDetailActivity.kt
@@ -0,0 +1,29 @@
+package com.hpr.becuniversity
+
+import android.os.Bundle
+import android.app.Activity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_show_detail.*
+
+class ShowDetailActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_detail)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+}
