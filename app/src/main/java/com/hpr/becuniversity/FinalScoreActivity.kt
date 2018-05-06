4af232ff17667ee8bf524c6836135967aef8aec5
Paulo Linhares <paulolinhares@mbp-de-macbook.intra.fazenda.sp.gov.br>
2018-05-06 11:16:35 -0300

    done

--- app/src/main/java/com/hpr/becuniversity/FinalScoreActivity.kt
+++ app/src/main/java/com/hpr/becuniversity/FinalScoreActivity.kt
@@ -2,11 +2,14 @@ package com.hpr.becuniversity
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.animation.SpringAnimation
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_final_score.*
+import android.support.animation.SpringForce
+import kotlinx.android.synthetic.main.content_final_score.*
+
 
 class FinalScoreActivity : AppCompatActivity() {
 
@@ -28,6 +31,19 @@ class FinalScoreActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        var springY = SpringAnimation(llScoreValue, SpringAnimation.ROTATION_Y, 0f)
+
+        val springForceX = SpringForce()
+        springForceX.stiffness = SpringForce.STIFFNESS_MEDIUM
+        springForceX.dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+        springY.setSpring(springForceX)
+        springY.start()
+
     }
 
 }
