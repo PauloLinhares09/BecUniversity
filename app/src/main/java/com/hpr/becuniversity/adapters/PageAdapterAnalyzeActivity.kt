1d14890762ce81392e01acf18818bcf16dc7d285
Paulo Linhares <paulolinhares@mbp-de-macbook.intra.fazenda.sp.gov.br>
2018-05-06 02:14:01 -0300

    vector icons in tab ok

--- app/src/main/java/com/hpr/becuniversity/adapters/PageAdapterAnalyzeActivity.kt
+++ app/src/main/java/com/hpr/becuniversity/adapters/PageAdapterAnalyzeActivity.kt
@@ -3,8 +3,6 @@ package com.hpr.becuniversity.adapters
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import android.support.v4.view.PagerAdapter
-import android.view.View
 import com.hpr.becuniversity.fragments.Tab1AnalyzeFragment
 import com.hpr.becuniversity.fragments.Tab2AnalyzeFragment
 
